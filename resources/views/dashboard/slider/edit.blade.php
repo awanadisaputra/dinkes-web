7ccebac22ad5dd56b6f8e7d4a4bb5eb891a3064c
Dennis <denn.ny173@gmail.com>
2026-01-12 10:12:46 +0700

    feat: simplify slider rich text editor for consistency with other features

--- resources/views/dashboard/slider/edit.blade.php
+++ resources/views/dashboard/slider/edit.blade.php
@@ -91,20 +91,13 @@
                 placeholder: 'Tulis konten lengkap di sini...',
                 modules: {
                     toolbar: [
+                        [{ 'header': [1, 2, 3, false] }],
                         ['bold', 'italic', 'underline', 'strike'],
                         ['blockquote', 'code-block'],
-                        [{ 'header': 1 }, { 'header': 2 }],
-                        [{ 'list': 'ordered' }, { 'list': 'bullet' }],
-                        [{ 'script': 'sub' }, { 'script': 'super' }],
-                        [{ 'indent': '-1' }, { 'indent': '+1' }],
-                        [{ 'direction': 'rtl' }],
-                        [{ 'size': ['small', false, 'large', 'huge'] }],
-                        [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
-                        [{ 'color': [] }, { 'background': [] }],
-                        [{ 'font': [] }],
+                        [{ 'list': 'ordered'}, { 'list': 'bullet' }],
                         [{ 'align': [] }],
-                        ['clean'],
-                        ['link', 'image']
+                        ['link', 'image'],
+                        ['clean']
                     ]
                 }
             });
